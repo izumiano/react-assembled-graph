4283a721d0a78b168cee58f5fddccf4862b6bd4a
izumiano <izumiano@users.noreply.github.com>
2026-01-03 20:23:35 +0100

    add eruda console for mobile debugging

--- example/main.tsx
+++ example/main.tsx
@@ -3,6 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
+if (
+	import.meta.env.MODE === "development" &&
+	/\?console/.test(window.location.href)
+) {
+	import("eruda").then((eruda) => eruda.default.init());
+}
+
 // biome-ignore lint/style/noNonNullAssertion: <root will always exist>
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>

0e71f017638c55998ff35b4147eb11a6ee187fa7
izumiano <izumiano@users.noreply.github.com>
2026-01-12 08:29:58 +0100

    better eruda control

--- example/main.tsx
+++ example/main.tsx
@@ -3,11 +3,20 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
-if (
-	import.meta.env.MODE === "development" &&
-	/\?console/.test(window.location.href)
-) {
-	import("eruda").then((eruda) => eruda.default.init());
+if (import.meta.env.DEV) {
+	const matches = window.location.href.match(
+		/\?console(?:=(?<config>show|hide))?/,
+	);
+	if (matches) {
+		import("eruda").then((_eruda) => {
+			const eruda = _eruda.default;
+			eruda.init();
+
+			if (matches.groups?.config === "show") {
+				eruda.show();
+			}
+		});
+	}
 }
 
 // biome-ignore lint/style/noNonNullAssertion: <root will always exist>
